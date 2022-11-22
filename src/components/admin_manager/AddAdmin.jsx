bf836d319e377f7caa62dde1a92b2b685641dee3
DevGeraldR <romerogerald22@gmail.com>
2022-11-23 00:29:34 +0800

    Add admin realtime upadate added

--- src/components/admin_manager/AddAdmin.jsx
+++ src/components/admin_manager/AddAdmin.jsx
@@ -2,15 +2,16 @@ import {
   collection,
   deleteDoc,
   doc,
-  getDocs,
+  onSnapshot,
   updateDoc,
 } from "firebase/firestore";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { db } from "../firebase/firebase";
 
 function AddAdmin() {
   const [admins, setAdmins] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { logOut } = useAuth();
 
   const handleClickLogOut = async () => {
@@ -29,39 +30,32 @@ function AddAdmin() {
     const adminRef = doc(db, "Requesting Admins", email);
     await deleteDoc(adminRef);
 
-    //To delete the admin from the admins array to rerender display without the added admin
-    setAdmins(admins.filter((admin) => admin.adminEmail !== email));
-
     alert("Admins sucessfully added");
   };
 
   useEffect(() => {
-    //To get and store the list of hospital from our database to the variables.
-    (async () => {
-      const querySnapshot = await getDocs(collection(db, "Requesting Admins"));
-      const admins = [];
-      querySnapshot.forEach((doc) => {
-        const { adminName, adminEmail, street, city, province, contactNumber } =
-          doc.data();
-        admins.push({
-          id: doc.id,
-          adminName,
-          adminEmail,
-          street,
-          city,
-          province,
-          contactNumber,
+    //Use to listen to the database and get the new
+    const unsub = onSnapshot(
+      collection(db, "Requesting Admins"),
+      (snapshot) => {
+        const admin = [];
+        snapshot.forEach((doc) => {
+          admin.push(doc.data());
         });
-      });
-      setAdmins(admins);
-    })();
+        setAdmins(admin);
+      }
+    );
+    setLoading(false);
+    return () => unsub();
   }, []);
 
-  return (
+  return loading ? (
+    <h1>Loading pa po....</h1>
+  ) : (
     <div className="flex items-center flex-col h-screen w-full bg-[#ebf2f3] overflow-auto">
       {admins.map((admin) => (
         <div
-          key={admin.id}
+          key={admin.adminEmail}
           className="max-w-[800px] m-2 flex flex-row border border-solid p-3 w-full border-black justify-between"
         >
           <div>{admin.adminName}</div>
